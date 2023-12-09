facf751bfd0767b03fcfc3f0a338878a52d46c0a
Rakshitasinghh <rakshita.singhhh@gmail.com>
2023-12-09 18:11:25 +0530

    initial commit

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const String google_api_key = "AIzaSyAgdbLEN1z_TmGZZNLLKd24xMEZa2yV8fQ";
+const Color primaryColor = Color(0xFF7B61FF);
+const double defaultPadding = 16.0;
