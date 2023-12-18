c3742ddd92feaeed61c14da54996832609911088
Rakshitasinghh <rakshita.singhhh@gmail.com>
2023-12-18 20:27:08 +0530

    submitted places page

---
+++ AppDev11/android/app/src/main/kotlin/com/example/appdeb51/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.appdeb51
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ AppDev11/lib/Place_Page.dart
@@ -0,0 +1,390 @@
+
+import 'package:flutter/material.dart';
+
+class MyPlace extends StatefulWidget {
+  const MyPlace({super.key});
+
+  @override
+  State<MyPlace> createState() => _MyPlaceState();
+}
+
+class _MyPlaceState extends State<MyPlace> {
+  @override
+  Widget build(BuildContext context) {
+    final double height=MediaQuery.of(context).size.height;
+    final double width=MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children:[
+          //-------------------HEADING-----------------------
+          Container(
+            height: 230,
+            decoration: const BoxDecoration(
+                borderRadius:  BorderRadius.only(
+                  bottomRight: Radius.circular(50),
+                ),
+                color: Color.fromRGBO(102, 107, 237, 1),
+                boxShadow:  [
+                  BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(-10.0,10.0),
+                      blurRadius: 20.0,
+                      spreadRadius: 4.0
+
+                  )
+                ]
+            ),
+            child: Stack(
+              children: [
+                Positioned(
+                    left: 0,
+                    top: 80,
+                    child: Container(
+                      height: 100,
+                      width: 300,
+                      decoration:const BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(50),
+                          bottomRight: Radius.circular(50),
+                        ),
+                        color: Colors.white,
+
+                      ),
+
+                    )
+                ),
+                const Positioned(
+                    top: 100,
+                    left: 20,
+                    child: Text('Places', style: TextStyle(
+                      fontSize: 40, color: Colors.deepPurpleAccent,
+                      fontWeight: FontWeight.w900,
+                    ),))
+              ],
+            ),
+          ),
+          SizedBox(height: height*0.05),
+          //-------------------CLUBS DIVISIONS---------------
+
+          Expanded(
+              child:MediaQuery.removePadding(context: context,
+                  removeTop: true,
+                  child: ListView(
+                      children: [
+                        Container(
+                          height: 230,
+                          child: Stack(
+                            children: [
+                              Positioned(
+                                  top: 35,
+                                  left: 20,
+                                  child: Material(
+
+                                    child: Container(
+                                      height: 180,
+                                      width: width*0.9,
+                                      decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(0.0),
+                                        boxShadow:const [
+                                          BoxShadow(
+                                              color: Colors.grey,
+                                              offset: Offset(-10.0,10.0),
+                                              blurRadius: 20.0,
+                                              spreadRadius: 4.0
+                                          )
+                                        ],
+                                      ),
+                                    ),
+
+                                  )),
+                              Positioned(
+                                top: 0,
+                                left: 30,
+                                child: Card(
+                                  elevation: 10.0,
+                                  shadowColor: Colors.grey.withOpacity(0.5),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(15.0),
+                                  ),
+                                  child: Container(
+                                    height: 200,
+                                    width: 150,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10.0),
+                                        image: const DecorationImage(
+                                          fit: BoxFit.fill,
+                                          image: AssetImage('assets/img.png'),
+                                        )
+                                    ),
+                                  ),
+
+                                ),
+                              ),
+                              Positioned(
+                                  top: 45,
+                                  left: 200,
+                                  child: Container(
+                                    height: 150,
+                                    width: 180,
+                                    child: const Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text("Amulya",style: TextStyle(fontSize: 25,
+                                            color: Colors.black, fontWeight: FontWeight.bold
+                                        ),),
+                                        Text("Resturant",style: TextStyle(fontSize: 20,
+                                            color: Colors.black, fontWeight: FontWeight.w500
+                                        ),),
+                                        Divider(color: Colors.black),
+                                        Text("Bellahali",style: TextStyle(fontSize: 20,
+                                            color: Colors.black, fontWeight: FontWeight.bold
+                                        ),)
+
+                                      ],
+                                    ),
+                                  ))
+                            ],
+                          ),
+                        ),
+                        Container(
+                          height: 230,
+                          child: Stack(
+                            children: [
+                              Positioned(
+                                  top: 35,
+                                  left: 20,
+                                  child: Material(
+
+                                    child: Container(
+                                      height: 180,
+                                      width: width*0.9,
+                                      decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(0.0),
+                                        boxShadow:const [
+                                          BoxShadow(
+                                              color: Colors.grey,
+                                              offset: Offset(-10.0,10.0),
+                                              blurRadius: 20.0,
+                                              spreadRadius: 4.0
+                                          )
+                                        ],
+                                      ),
+                                    ),
+
+                                  )),
+                              Positioned(
+                                top: 0,
+                                left: 30,
+                                child: Card(
+                                  elevation: 10.0,
+                                  shadowColor: Colors.grey.withOpacity(0.5),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(15.0),
+                                  ),
+                                  child: Container(
+                                    height: 200,
+                                    width: 150,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10.0),
+                                        image: const DecorationImage(
+                                          fit: BoxFit.fill,
+                                          image: AssetImage('assets/img_1.png'),
+                                        )
+                                    ),
+                                  ),
+
+                                ),
+                              ),
+                              Positioned(
+                                  top: 45,
+                                  left: 200,
+                                  child: Container(
+                                    height: 150,
+                                    width: 180,
+                                    child: const Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text("Church Street",style: TextStyle(fontSize: 25,
+                                            color: Colors.black, fontWeight: FontWeight.bold
+                                        ),),
+                                        Text("Food and Nightlife",style: TextStyle(fontSize: 20,
+                                            color: Colors.black, fontWeight: FontWeight.w500
+                                        ),),
+                                        Divider(color: Colors.black),
+                                        Text("Central banglore",style: TextStyle(fontSize: 20,
+                                            color: Colors.black, fontWeight: FontWeight.bold
+                                        ),)
+
+                                      ],
+                                    ),
+                                  ))
+                            ],
+                          ),
+                        ),
+                        Container(
+                          height: 230,
+                          child: Stack(
+                            children: [
+                              Positioned(
+                                  top: 35,
+                                  left: 20,
+                                  child: Material(
+
+                                    child: Container(
+                                      height: 180,
+                                      width: width*0.9,
+                                      decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(0.0),
+                                        boxShadow:const [
+                                          BoxShadow(
+                                              color: Colors.grey,
+                                              offset: Offset(-10.0,10.0),
+                                              blurRadius: 20.0,
+                                              spreadRadius: 4.0
+                                          )
+                                        ],
+                                      ),
+                                    ),
+
+                                  )),
+                              Positioned(
+                                top: 0,
+                                left: 30,
+                                child: Card(
+                                  elevation: 10.0,
+                                  shadowColor: Colors.grey.withOpacity(0.5),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(15.0),
+                                  ),
+                                  child: Container(
+                                    height: 200,
+                                    width: 150,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10.0),
+                                        image: const DecorationImage(
+                                          fit: BoxFit.fill,
+                                          image: AssetImage('assets/img_2.png'),
+                                        )
+                                    ),
+                                  ),
+
+                                ),
+                              ),
+                              Positioned(
+                                  top: 45,
+                                  left: 200,
+                                  child: Container(
+                                    height: 150,
+                                    width: 180,
+                                    child: const Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text("Lake",style: TextStyle(fontSize: 25,
+                                            color: Colors.black, fontWeight: FontWeight.bold
+                                        ),),
+                                        Text("Tourist Place",style: TextStyle(fontSize: 20,
+                                            color: Colors.black, fontWeight: FontWeight.w500
+                                        ),),
+                                        Divider(color: Colors.black),
+                                        Text("kattigenahalli",style: TextStyle(fontSize: 20,
+                                            color: Colors.black, fontWeight: FontWeight.bold
+                                        ),)
+
+                                      ],
+                                    ),
+                                  ))
+                            ],
+                          ),
+                        ),
+                        Container(
+                          height: 230,
+                          child: Stack(
+                            children: [
+                              Positioned(
+                                  top: 35,
+                                  left: 20,
+                                  child: Material(
+
+                                    child: Container(
+                                      height: 180,
+                                      width: width*0.9,
+                                      decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(0.0),
+                                        boxShadow:const [
+                                          BoxShadow(
+                                              color: Colors.grey,
+                                              offset: Offset(-10.0,10.0),
+                                              blurRadius: 20.0,
+                                              spreadRadius: 4.0
+                                          )
+                                        ],
+                                      ),
+                                    ),
+
+                                  )),
+                              Positioned(
+                                top: 0,
+                                left: 30,
+                                child: Card(
+                                  elevation: 10.0,
+                                  shadowColor: Colors.grey.withOpacity(0.5),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(15.0),
+                                  ),
+                                  child: Container(
+                                    height: 200,
+                                    width: 150,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10.0),
+                                        image: const DecorationImage(
+                                          fit: BoxFit.fill,
+                                          image: AssetImage('assets/img_4.png'),
+                                        )
+                                    ),
+                                  ),
+
+                                ),
+                              ),
+                              Positioned(
+                                  top: 45,
+                                  left: 200,
+                                  child: Container(
+                                    height: 150,
+                                    width: 180,
+                                    child: const Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text("GoKarting",style: TextStyle(fontSize: 25,
+                                            color: Colors.black, fontWeight: FontWeight.bold
+                                        ),),
+                                        Text("Games and adventure",style: TextStyle(fontSize: 20,
+                                            color: Colors.black, fontWeight: FontWeight.w500
+                                        ),),
+                                        Divider(color: Colors.black),
+                                        Text("Huskur",style: TextStyle(fontSize: 20,
+                                            color: Colors.black, fontWeight: FontWeight.bold
+                                        ),)
+
+                                      ],
+                                    ),
+                                  ))
+                            ],
+                          ),
+                        ),
+
+                      ]
+                  )
+              )
+          ),
+
+        ],
+      ),
+    );
+  }
+}
